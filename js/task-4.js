8865bcb125d88ff3b7fbe8521da6ed412fefa3fe
Andrii <andreymit123@gmail.com>
2026-02-20 16:04:00 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,43 @@
+const form = document.querySelector('form.login-form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const inputs = form.querySelectorAll('input');
+  for (const input of inputs) {
+  if (input.value.trim() === '') {
+    alert('All form fields must be filled in') } else {
+return
+    }
+}
+}
+  const user = {
+    const email = inputs[0].value.trim();
+    const password = inputs[1].value.trim();
+
+})
+
+// =================
+
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const inputs = form.querySelectorAll('input');
+
+  for (const input of inputs) {
+    if (input.value.trim() === '') {
+      alert('All form fields must be filled in');
+      return;
+    }
+  }
+
+  const user = {
+    email: inputs[0].value.trim(),
+    password: inputs[1].value.trim()
+  };
+
+  console.log(user);
+});
+
 
