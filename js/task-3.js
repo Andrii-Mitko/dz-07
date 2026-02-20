059861f14b68a5c3a58978558f4f0af6688781ef
Andrii-Mitko <andreymit123@gmail.com>
2026-02-20 23:43:53 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,10 @@
 const input = document.querySelector('#name-input');
-const output1= document.querySelector('#name-output');
-input.addEventListener(input, foo) {
-    
-}
+const output= document.querySelector('#name-output');
+input.addEventListener('input', () => {
+    if (input.value.trim() === '') {
+        output.textContent = 'Anonymous';
+    } else {
+     output.textContent = input.value.trim();
+    }
+})
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,43 +1,21 @@
+
+
+
 const form = document.querySelector('form.login-form');
+form.elements.email.setAttribute('autocomplete', 'username');
+form.elements.password.setAttribute('autocomplete', 'current-password');
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  const inputs = form.querySelectorAll('input');
-  for (const input of inputs) {
-  if (input.value.trim() === '') {
-    alert('All form fields must be filled in') } else {
-return
+  e.preventDefault(); 
+  if (form.elements.email.value.trim() === '' || form.elements.password.value.trim() === '') {
+    alert('All form fields must be filled in');
+    return;
+  } else {
+    const user = {
+    email: form.elements.email.value.trim(),
+    password: form.elements.password.value.trim(),
     }
-}
-}
-  const user = {
-    const email = inputs[0].value.trim();
-    const password = inputs[1].value.trim();
-
-})
-
-// =================
-
-
-const form = document.querySelector('.login-form');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const inputs = form.querySelectorAll('input');
-
-  for (const input of inputs) {
-    if (input.value.trim() === '') {
-      alert('All form fields must be filled in');
-      return;
-    }
+    console.log(user);
   }
-
-  const user = {
-    email: inputs[0].value.trim(),
-    password: inputs[1].value.trim()
-  };
-
-  console.log(user);
 });
 
 
